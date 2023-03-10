2409020e4265b4afa502b0973554915d3e0cf83c
Raphael aka theScripter <73601567+Oraclon@users.noreply.github.com>
2023-03-10 12:27:38 +0000

    NAS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MagCompComponent } from './mag-comp/mag-comp.component';
-import { NasCompComponent } from './nas-comp/nas-comp.component';
-import { NasSecondComponent } from './nasSecond/nasSecond.component';
-import { NasThirdComponent } from './nasThird/nasThird.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MagCompComponent,
-    NasCompComponent,
-    NasSecondComponent,
-    NasThirdComponent
+    AppComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
+
   providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
